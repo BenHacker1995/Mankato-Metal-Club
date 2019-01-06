47cbcf17fc1ee93b7e5c40c5dd89cc92d79bebed
Ben Hacker <bhacker1995@gmail.com>
2019-01-06 16:50:37 -0600

    Basic login form set up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,110 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { triggerLogin, formError, clearError } from '../../redux/actions/loginActions';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField'
+import Slide from '@material-ui/core/Slide';
 
-import { USER_ACTIONS } from '../../redux/actions/userActions';
-import { triggerLogout } from '../../redux/actions/loginActions';
+const mapStateToProps = state => ({
+  user: state.user,
+  login: state.login,
+});
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  TransitionUp(props) {
+    return <Slide direction="up" {...props} />;
+  }
+
+  componentDidMount() {
+    this.props.dispatch(clearError());
+  }
 
   
-  // componentDidMount() {
-  //   this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
-  // }
-
-    render() {
-        return (
-            <div>
-                <h1>Login</h1>
-                <form>
-
-                </form>
-            </div>
-        );
-      }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.user.userName) {
+    //   this.props.history.push('/char');
     }
+  }
 
-export default connect()( Login );
+  login = () => {
+
+    if (this.state.username === '' || this.state.password === '') {
+      this.props.dispatch(formError());
+    } else {
+      this.props.dispatch(triggerLogin(this.state.username, this.state.password));
+    }
+  }
+
+  handleInputChangeFor = propertyName => (event) => {
+    this.setState({
+      [propertyName]: event.target.value,
+    });
+  }
+
+  renderAlert() {
+    if (this.props.login.message !== '') {
+      return (
+        <h2
+          className="alert"
+          role="alert"
+        >
+          { this.props.login.message }
+        </h2>
+      );
+    }
+    return (<span />);
+  }
+
+  render() {
+    return (
+      <div>
+        { this.renderAlert() }
+        <Paper className='paper' TransitionComponent={this.TransitionUp}>
+          <h1>Login</h1>
+          <div>
+              <TextField
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={this.state.username}
+                onChange={this.handleInputChangeFor('username')}
+              />
+          </div>
+          <div>
+              <TextField
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleInputChangeFor('password')}
+              />
+          </div>
+          <div>
+            <Button
+              name="submit"
+              value="Log In"
+            //   onClick={ this.getChars }
+            >
+              Log In
+            </Button>
+            <Button>
+            <Link to="/register">Register</Link>
+            </Button>
+          </div>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Login);
